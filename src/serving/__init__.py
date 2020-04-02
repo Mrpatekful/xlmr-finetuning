5effb8d5f0b0ca67d6e3a037a67c6396988ee10c
Mrpatekful <prugai.patrik@gmail.com>
2020-04-02 15:33:39 +0200

    Minor typo fixes

--- src/serving/__init__.py
+++ src/serving/__init__.py
@@ -1,6 +1,6 @@
 """
 @author:    Patrik Purgai
-@copyright: Copyright 2019, entity-recognition
+@copyright: Copyright 2019, xlmr-finetuning 
 @license:   MIT
 @email:     purgai.patrik@gmail.com
 @date:      2019.09.30.
